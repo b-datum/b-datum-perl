ef7af08a724a41117d078cc51b2ac0ce561f89b8
renato <renato@aware.com.br>
2012-11-07 16:34:24 -0200

    prototype do BDatum::Simple::API::Node

--- lib/BDatum/Simple/API/Node.pm
+++ lib/BDatum/Simple/API/Node.pm
@@ -3,4 +3,37 @@ package BDatum::Simple::API::Node;
 use Moose;
 
 
+has 'partner_key' => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1
+);
+
+has 'node_key' => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1
+);
+
+sub send {
+
+}
+
+sub download {
+
+}
+
+sub delete {
+
+}
+
+sub list {
+
+}
+
+sub info {
+
+}
+
+
 1;
